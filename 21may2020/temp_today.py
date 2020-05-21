ea0b99f827a715b33970c602f4b72417b59aa63d
kavya-mm <kavyamarigowda12@gmail.com>
2020-05-21 19:06:33 +0530

    second

---
+++ 21may2020/temp_today.py
@@ -0,0 +1,13 @@
+import time
+import os
+import pandas 
+
+while True:
+    if os.path.exists("file/temp_today.csv"):
+        data = pandas.read_csv("file/temp_today.csv")
+        print(data.mean()[st1])
+        
+    else:
+        print("file does not exist.")    
+        
+    time.sleep(10)
---
+++ 21may2020/textfile.py
@@ -0,0 +1,11 @@
+myfile = open ("file/fruits.txt")
+content = myfile.read()
+myfile.close()
+
+with open("file/fruits.txt") as myfile:
+    content = myfile.read()
+
+print(content)
+
+with open("file/vegitables.txt" , "w") as myfile:
+    myfile.write("tomato\ncucumber\nOnion")
